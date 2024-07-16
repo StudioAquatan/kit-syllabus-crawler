0e66a982fcb2bc19612cf5e179215ee930c73d8e
f0reachARR <f0reach@f0reach.me>
2024-07-16 21:50:03 +0900

    Add elasticsearch base configration

---
+++ src/elasticsearch/mapping.ts
@@ -0,0 +1,180 @@
+import {
+  IndicesCreateRequest,
+  IndicesIndexSettings,
+  MappingTypeMapping,
+} from '@elastic/elasticsearch/lib/api/types';
+
+const indexSetting: IndicesIndexSettings = {
+  index: {
+    analysis: {
+      analyzer: {
+        kuromoji_normalize: {
+          type: 'custom',
+          char_filter: ['icu_normalizer'],
+          tokenizer: 'kuromoji_tokenizer',
+          filter: [
+            'kuromoji_baseform',
+            'kuromoji_part_of_speech',
+            'cjk_width',
+            'ja_stop',
+            'kuromoji_stemmer',
+            'lowercase',
+          ],
+        },
+      },
+    },
+  },
+};
+
+const subjectMapping: MappingTypeMapping['properties'] = {
+  id: {
+    type: 'integer',
+  },
+  timetableId: {
+    type: 'integer',
+  },
+  courseId: {
+    type: 'integer',
+  },
+  credits: {
+    type: 'integer',
+  },
+  code: {
+    type: 'keyword',
+    index: false,
+  },
+  flags: {
+    type: 'keyword',
+  },
+  categories: {
+    type: 'nested',
+    properties: {
+      available: {
+        type: 'boolean',
+      },
+      year: {
+        type: 'integer',
+      },
+      semester: {
+        type: 'keyword',
+      },
+      faculty: {
+        type: 'keyword',
+      },
+      field: {
+        type: 'keyword',
+      },
+      program: {
+        type: 'keyword',
+      },
+      category: {
+        type: 'keyword',
+      },
+      schedule: {
+        type: 'object',
+        properties: {
+          type: {
+            type: 'keyword',
+          },
+          days: {
+            type: 'nested',
+            properties: {
+              date: {
+                type: 'integer',
+              },
+              hour: {
+                type: 'integer',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  type: {
+    type: 'keyword',
+  },
+  class: {
+    type: 'keyword',
+  },
+  title: {
+    type: 'search_as_you_type',
+  },
+  instructors: {
+    type: 'nested',
+    properties: {
+      id: {
+        type: 'keyword',
+      },
+      name: {
+        type: 'text',
+      },
+    },
+  },
+  outline: {
+    type: 'text',
+  },
+  purpose: {
+    type: 'text',
+  },
+  requirement: {
+    type: 'text',
+  },
+  point: {
+    type: 'text',
+  },
+  textbook: {
+    type: 'text',
+  },
+  gradingPolicy: {
+    type: 'text',
+  },
+  remark: {
+    type: 'text',
+  },
+  researchPlan: {
+    type: 'text',
+  },
+  plans: {
+    type: 'nested',
+    properties: {
+      topic: {
+        type: 'text',
+      },
+      content: {
+        type: 'text',
+      },
+    },
+  },
+  goal: {
+    type: 'object',
+    properties: {
+      description: {
+        type: 'text',
+      },
+      evaluations: {
+        type: 'nested',
+        properties: {
+          label: {
+            type: 'text',
+          },
+          description: {
+            type: 'text',
+          },
+        },
+      },
+    },
+  },
+};
+
+export function createCreateIndexRequest(
+  indexName: string,
+): IndicesCreateRequest {
+  return {
+    index: indexName,
+    settings: indexSetting,
+    mappings: {
+      properties: subjectMapping,
+    },
+  };
+}
